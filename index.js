923656c0c7e215dd70e7bf74ddfbe0fc31d0d8b0
adrputra <ricardoandra17@gmail.com>
2022-09-07 21:53:54 +0700

    Bot V2

--- index.js
+++ index.js
@@ -8,30 +8,40 @@ const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
 
-const client = new Client({
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {
+      root: __dirname
+    });
+  });
+
+  const client = new Client({
+    restartOnAuthFail: true,
     puppeteer: {
-        headless: true,
-        args: [
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-          '--disable-dev-shm-usage',
-          '--disable-accelerated-2d-canvas',
-          '--no-first-run',
-          '--no-zygote',
-          '--disable-gpu'
-        ],
-      },
-    authStrategy: new LocalAuth()
-});
-
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
-});
-
-client.on('ready', () => {
-    console.log('Client is ready!');
-});
-
+      headless: true,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--no-first-run',
+        '--no-zygote',
+        '--disable-gpu'
+      ],
+    },
+    authStrategy: new LocalAuth({ clientId: "confess-bot" })
+  });
 
 
 var receiver = [];
@@ -125,3 +135,41 @@ client.on('message', message => {
 });
 
 client.initialize();
+
+// Socket IO
+io.on('connection', function(socket) {
+    socket.emit('message', 'Connecting...');
+  
+    client.on('qr', (qr) => {
+      console.log('QR RECEIVED', qr);
+      qrcode.toDataURL(qr, (err, url) => {
+        socket.emit('qr', url);
+        socket.emit('message', 'QR Code received, scan please!');
+      });
+    });
+  
+    client.on('ready', () => {
+      socket.emit('ready', 'Whatsapp is ready!');
+      socket.emit('message', 'Whatsapp is ready!');
+    });
+  
+    client.on('authenticated', () => {
+      socket.emit('authenticated', 'Whatsapp is authenticated!');
+      socket.emit('message', 'Whatsapp is authenticated!');
+      console.log('AUTHENTICATED');
+    });
+  
+    client.on('auth_failure', function(session) {
+      socket.emit('message', 'Auth failure, restarting...');
+    });
+  
+    client.on('disconnected', (reason) => {
+      socket.emit('message', 'Whatsapp is disconnected!');
+      client.destroy();
+      client.initialize();
+    });
+  });
+
+  server.listen(port, function() {
+    console.log('App running on *: ' + port);
+  });
